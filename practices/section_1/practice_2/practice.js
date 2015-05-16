b411d0beb3d9f39f97dc73e8aac8ef8947a2ee88
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-16 21:16:19 +0800

    completion of the S1/P3,update S1/P2

--- practices/section_1/practice_2/practice.js
+++ practices/section_1/practice_2/practice.js
@@ -3,7 +3,7 @@ function collect_same_elements(collection_a, collection_b) {
   var same=[];//创建一个数组，来存储重复的数据。
   for (i=0;i<collection_a.length;i++){
     for(x=0;x<collection_b.length;x++){
-      //涉及到数组的嵌套，这个for循环本可以省去，但如果改数组长度变化，肯能造成Bug
+      //涉及到数组的嵌套，这个for循环本可以省去，但如果改数组长度变化，可能造成Bug
       for(j=0;j<collection_b[x].length;j++){
         if(collection_a[i]==collection_b[x][j]){
           same.push(collection_a[i]);
--- practices/section_1/practice_3/practice.js
+++ practices/section_1/practice_3/practice.js
@@ -1,5 +1,15 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var same=[];//创建一个数组，来存储重复的数据。
+  var value=object_b.value;//取出object_b.value的值，存储用于比较。
+  for (i=0;i<collection_a.length;i++){
+    for(j=0;j<value.length;j++){
+      if(collection_a[i]==value[j]){
+        same.push(collection_a[i]);
+      }
+    }
+  }
+  return same;
 }
 
 module.exports = collect_same_elements;
