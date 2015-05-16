8067845d30376df7f64d20ba09aed797a5d01180
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-17 01:02:31 +0800

    complete section_1&S2/P1

--- practices/section_1/practice_4/practice.js
+++ practices/section_1/practice_4/practice.js
@@ -1,5 +1,22 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var same=[];//创建一个数组，来存储重复的数据。
+  var key=[];//创建一个数组，迎来存储collection_a中需要比较的数据。
+  var value=object_b.value;//创建一个数组，迎来存储object_b中需要比较的数据。
+  for(x=0;x<9;x++){
+    key.push(collection_a[x].key);
+    console.log(key);
+  }
+
+  for (i=0;i<key.length;i++){
+    for(j=0;j<value.length;j++){
+      if(key[i]==value[j]){
+        same.push(key[i]);
+      }
+    }
+  }
+
+  return same;
 }
 
 module.exports = collect_same_elements;
--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,26 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var sum=['a','e','h','t','f','c','g','b','d'];
+  var sum_info=[
+    {key: "a", count: 0},
+    {key: "e", count: 0},
+    {key: "h", count: 0},
+    {key: "t", count: 0},
+    {key: "f", count: 0},
+    {key: "c", count: 0},
+    {key: "g", count: 0},
+    {key: "b", count: 0},
+    {key: "d", count: 0}
+  ];
+  for(i=0;i<sum_info.length;i++){
+    for(j=0;j<collection.length;j++){
+      if(sum_info[i].key==collection[j]){
+        sum_info[i].count=sum_info[i].count+1;
+      }
+
+    }
+  }
+  return sum_info;
 }
 
 module.exports = count_same_elements;
