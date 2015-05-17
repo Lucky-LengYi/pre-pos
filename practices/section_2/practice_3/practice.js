7d422c8eeb72f39edeada293d84fde47e311acdb
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-17 16:24:47 +0800

    finish homework S2/P3 has Bug

--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -4,18 +4,34 @@ function count_same_elements(collection) {
     var sum_list=[];//创建一个JSON数组来存放所有sum对象。
     var test=true;
     for (i=0;i<collection.length;i++){
+      if(i+1<collection.length){
+        num_b=collection[i+1].charAt(0);
+      }
+      num_a=collection[i].charAt(0);
       if(test){
-        sum={key:collection[i].charAt(0),count:0};
+        sum={name:collection[i].charAt(0),summary:0};
         //.charAt(0）是为了取出该数据的第一个字母。生成（替换上一条）一条sum数据。
         test=false;
       }
-      sum.count=sum.count+1;//统计相同字母的数量。
-      for(j=0;j<10;j++){
-        if(collection[i].indexOf(j)>-1){
-          sum.count=sum.count+j-1;
+      sum.summary=sum.summary+1;//统计相同字母的数量。
+//!!!!!!!!!!!BUG，统计数据时，发现t-2，-2会认定是负数，会进入负数的循环，
+//而d-5会进入正数和负数的循环，导致统计结果为d：10，所以将负数设定为-3
+//使得t-2可以进入循环而d-5不进入循环。
+      for(x=-3;x<0;x++){
+        if(collection[i].indexOf(x)>-1){
+          sum.summary=sum.summary+Math.abs(x);
         }
       }
-      if(collection[i]!=collection[i+1]){
+      for(z=0;z<11;z++){
+        if(collection[i].indexOf(z)>-1){
+          sum.summary=sum.summary+z-1;
+        }
+      }
+      if(num_a!=num_b){
+        sum_list.push(sum);//将sum数据存入sum_list中。
+        test=true;
+      }
+      if(i+1==collection.length){
         sum_list.push(sum);//将sum数据存入sum_list中。
         test=true;
       }
