728c530a0ac21d0fdd51523789cf93f463555753
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-05-18 13:49:27 +0800

    update section2 all file

--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,26 +1,32 @@
 function count_same_elements(collection) {
-  //在这里写入代码
-  var sum={};//创建一个sun object来存储每一条相同字母和数量
-  var sum_list=[];//创建一个JSON数组来存放所有sum对象。
-  var test=true;
-  for (i=0;i<collection.length;i++){
-    if(test){
-      sum={key:collection[i].charAt(0),count:0};
-      //.charAt(0）是为了取出该数据的第一个字母。生成（替换上一条）一条sum数据。
-      test=false;
-    }
-    sum.count=sum.count+1;//统计相同字母的数量。
-    for(y=0;y<10;y++){
-      if(collection[i].indexOf(y)>-1){
-        sum.count=sum.count+y-1;
-      }
-    }
-    if(collection[i]!=collection[i+1]){
-      sum_list.push(sum);//将sum数据存入sum_list中。
-      test=true;
-    }
-  }
-  return sum_list;
-}
+        //分阶段，空行
+        //for循环声明变量 （严格模式）
+        var sum_list=[];//创建一个JSON数组来存放所有sum对象。
+        for (var i=0;i<collection.length;i++){
 
+          var sum={};//创建一个sun object来存储每一条相同字母和数量
+          var is_exist=false;//is_exist
+
+          for (var x=0;x<sum_list.length;x++){
+            if (sum_list[x].key===collection[i].substring(0, 1)){
+              if (collection[i].length > 2){
+                sum_list[x].count=sum_list[x].count+parseInt(collection[i].substring(2));
+              }else{
+                sum_list[x].count++;
+              }
+              is_exist = true;
+            }
+          }
+
+          if (!is_exist){
+            if (collection[i].length > 2){
+              sum={key:collection[i].charAt(0),count:parseInt(collection[i].substring(2))};
+            }else{
+              sum={key:collection[i].charAt(0),count:1};
+            }
+            sum_list.push(sum);
+          }
+        }
+        return sum_list;
+    }
 module.exports = count_same_elements;
--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,41 +1,32 @@
 function count_same_elements(collection) {
-    //在这里写入代码
-    var sum={};//创建一个sun object来存储每一条相同字母和数量
-    var sum_list=[];//创建一个JSON数组来存放所有sum对象。
-    var test=true;
-    for (i=0;i<collection.length;i++){
-      if(i+1<collection.length){
-        num_b=collection[i+1].charAt(0);
-      }
-      num_a=collection[i].charAt(0);
-      if(test){
-        sum={name:collection[i].charAt(0),summary:0};
-        //.charAt(0）是为了取出该数据的第一个字母。生成（替换上一条）一条sum数据。
-        test=false;
-      }
-      sum.summary=sum.summary+1;//统计相同字母的数量。
-//!!!!!!!!!!!BUG，统计数据时，发现t-2，-2会认定是负数，会进入负数的循环，
-//而d-5会进入正数和负数的循环，导致统计结果为d：10，所以将负数设定为-3
-//使得t-2可以进入循环而d-5不进入循环。
-      for(x=-3;x<0;x++){
-        if(collection[i].indexOf(x)>-1){
-          sum.summary=sum.summary+Math.abs(x);
+        //分阶段，空行
+        //for循环声明变量 （严格模式）
+        var sum_list=[];//创建一个JSON数组来存放所有sum对象。
+        for (var i=0;i<collection.length;i++){
+
+          var sum={};//创建一个sun object来存储每一条相同字母和数量
+          var is_exist=false;//is_exist
+
+          for (var x=0;x<sum_list.length;x++){
+            if (sum_list[x].name===collection[i].substring(0, 1)){
+              if (collection[i].length > 2){
+                sum_list[x].summary=sum_list[x].summary+parseInt(collection[i].substring(2));
+              }else{
+                sum_list[x].summary++;
+              }
+              is_exist = true;
+            }
+          }
+
+          if (!is_exist){
+            if (collection[i].length > 2){
+              sum={name:collection[i].charAt(0),summary:parseInt(collection[i].substring(2))};
+            }else{
+              sum={name:collection[i].charAt(0),summary:1};
+            }
+            sum_list.push(sum);
+          }
         }
-      }
-      for(z=0;z<11;z++){
-        if(collection[i].indexOf(z)>-1){
-          sum.summary=sum.summary+z-1;
-        }
-      }
-      if(num_a!=num_b){
-        sum_list.push(sum);//将sum数据存入sum_list中。
-        test=true;
-      }
-      if(i+1==collection.length){
-        sum_list.push(sum);//将sum数据存入sum_list中。
-        test=true;
-      }
+        return sum_list;
     }
-    return sum_list;
-  }
 module.exports = count_same_elements;
